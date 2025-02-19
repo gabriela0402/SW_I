49ba4c273bef6b70efc4256210c3997b12410476
gabriela0402 <161508311+gabriela0402@users.noreply.github.com>
2025-02-19 15:09:07 -0300

    Add files via upload

---
+++ ex03.php
@@ -0,0 +1,15 @@
+<?php
+    $v1=10;
+    $v2=100;
+    echo 'soma:'.$v1 + $v2;
+    echo '<br>';
+    echo 'subtração'.$v1 - $v2;
+    echo '<br>';
+    echo 'multiplicação:'.$v1 * $v2;
+    echo '<br>';
+    echo 'divisão:'.$v1 / $v2;
+    echo '<br>';
+    echo 'resto:'.$v1 % $v2;
+    echo '<br>';
+    
+?>
