a07c31e9ccd7b9d0cea2f51ad5f00b5a70756d3b
gabriela0402 <161508311+gabriela0402@users.noreply.github.com>
2025-02-28 18:18:10 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_GET['cxnome'];
+
+    echo "Bem Vindo, $nome";
+
+
+?>
---
+++ aula03/processaex01.php
@@ -0,0 +1,6 @@
+<?php
+    $nome= $_POST['cxnome'];
+    $mensagem= $_POST['cxmensagem'];
+
+    echo "Olá, $nome !A sua mensagem foi: $mensagem"
+?>
