da7b46c21095fab90ee5a64d1039dd5e05e40a26
gabriela0402 <161508311+gabriela0402@users.noreply.github.com>
2025-05-09 16:54:18 -0300

    Add files via upload

---
+++ aula09.02/Lampada.class.php
@@ -0,0 +1,31 @@
+<?php
+    class Lampada{
+        public $Fabricante;
+        public $Tensao;
+        public $Potencia;
+        public $Cor;
+        public $Status;
+
+        public function Ligar(){
+            $this->Status = True;
+        }
+
+        public function Desligar(){
+            $this->Status = False;
+        }
+
+        public function MostrarDados(){
+            echo "O Fabricante é:" . $this ->Fabricante . "<br>";
+            echo "A Tensao é:" . $this ->Tensao . "<br>";
+            echo "A Potencia é:" . $this ->Potencia . "<br>";
+            echo "A Cor é:" . $this->Cor . "<br>";
+            if ($this->Status == 1){
+                echo "O Status é: LIGADA <br>";
+            }
+            else{
+                echo"O status é: DESLIGADA <br>";
+            }
+            echo "<hr>";
+        }
+    }
+?>
---
+++ aula09.02/exemplo_lampada.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'Lampada.class.php';
+
+    $lamp1 = new Lampada();
+
+    // var_dump($lamp1); // Para ver oque tem dentro do objeto
+
+    $lamp1->Fabricante = "Philips";
+    $lamp1->Tensao = 110 ;
+    $lamp1->Potencia = 50;
+    $lamp1->Cor = "Branca";
+    
+    $lamp1->MostrarDados();
+
+    $lamp1->Ligar();
+    $lamp1->MostrarDados();
+
+?>
